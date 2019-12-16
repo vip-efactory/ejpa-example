5b6b9c799f4269da10c5b6c74851ab4f4bf762ae
dbdu <1797890817@qq.com>
2019-12-16 16:39:10 +0800

    实体属性的检查，已知问题：国际化文本里面的占位符还没有处理

--- src/main/java/vip/efactory/ejpa/example/entity/Student.java
+++ src/main/java/vip/efactory/ejpa/example/entity/Student.java
@@ -3,9 +3,15 @@ package vip.efactory.ejpa.example.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 import vip.efactory.ejpa.base.entity.BaseEntity;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.Date;
 
 /**
@@ -20,12 +26,25 @@ public class Student extends BaseEntity<Long> {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Length(min = 4, max = 8, message = "{student.name} {property.length.in.between}")
+    @NotBlank(message = "{student.name} {property.not.allow.empty}")
     private String name;
 
+    @Range(min = 0, max = 2, message = "{student.sex} {property.length.in.between}")
     private Integer sex;    // 0 未知；1 男；2 女
 
+    @PositiveOrZero(message = "{student.age} {property.not.allow.negative}")
     private Integer age;
 
+    /**
+     * 爱好
+     */
+    private String hobby;
+
+    @Email(message = "{student.email}{property.format.error}")
+    private String email;
+
+    @Past(message = "{student.birthday}{property.value.only.past}")
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     @Temporal(TemporalType.DATE)
     private Date birthday;
