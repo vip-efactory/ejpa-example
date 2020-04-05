4acd5af596caa4e6cb0cd5d957d1350d7bee1ea4
dbdu <1797890817@qq.com>
2020-04-05 16:13:04 +0800

    已知问题：租户数据过滤器对主键key直接获取不支持过滤，官方文档中明确提到这一点. Filters apply to entity queries, but not to direct fetching. https://docs.jboss.org/hibernate/orm/5.4/userguide/html_single/Hibernate_User_Guide.html#pc-filter

--- src/main/java/vip/efactory/ejpa/example/config/TenantHibernateInterceptor.java
+++ src/main/java/vip/efactory/ejpa/example/config/TenantHibernateInterceptor.java
@@ -1,14 +1,13 @@
 package vip.efactory.ejpa.example.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.CallbackException;
 import org.hibernate.EmptyInterceptor;
-import org.hibernate.EntityMode;
 import org.hibernate.type.Type;
 import vip.efactory.ejpa.tenant.column.TenantBaseEntity;
 import vip.efactory.ejpa.tenant.identifier.TenantHolder;
 
 import java.io.Serializable;
+import java.util.Iterator;
 
 /**
  * 重写hibernate的空拦截器的实现，对所有的实体操作都要进行租户条件过滤
@@ -48,83 +47,92 @@ public class TenantHibernateInterceptor extends EmptyInterceptor {
         }
     }
 
+    @Override
+    public void preFlush(Iterator entities) {
+        entities.forEachRemaining(entity -> {
+            if (entity instanceof TenantBaseEntity) {
+                log.debug("[pre-flush] Updating the entity with tenant information: " + TenantHolder.getTenantId());
+                ((TenantBaseEntity) entity).setTenantId(TenantHolder.getTenantId());
+            }
+        });
+    }
+
+//    /**
+//     * 清空flush时
+//     */
 //    @Override
-//    public void preFlush(Iterator entities) {
-//        entities.forEachRemaining(entity -> {
-//            if (entity instanceof TenantBaseEntity) {
-//                log.debug("[pre-flush] Updating the entity with tenant information: " + TenantHolder.getTenantId());
-//                ((TenantBaseEntity) entity).setTenantId(TenantHolder.getTenantId());
-//            }
-//        });
+//    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
+//        if (entity instanceof TenantBaseEntity) {
+//            log.debug("[flush-dirty] Updating the entity " + id + " with tenant information: " + TenantHolder.getTenantId());
+//            ((TenantBaseEntity) entity).setTenantId(TenantHolder.getTenantId());
+//        }
+//        return false;
 //    }
-
-    /**
-     * 清空flush时
-     */
-    @Override
-    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
-        if (entity instanceof TenantBaseEntity) {
-            log.debug("[flush-dirty] Updating the entity " + id + " with tenant information: " + TenantHolder.getTenantId());
-            ((TenantBaseEntity) entity).setTenantId(TenantHolder.getTenantId());
-        }
-        return false;
-    }
-
+//
+////    @Override
+////    public void postFlush(Iterator entities) {
+////        entities.forEachRemaining(entity -> {
+////            if (entity instanceof TenantBaseEntity) {
+////                log.debug("[post-flush] Updating the entity with tenant information: " + TenantHolder.getTenantId());
+////                ((TenantBaseEntity) entity).setTenantId(TenantHolder.getTenantId());
+////            }
+////        });
+////    }
+//
+//
 //    @Override
-//    public void postFlush(Iterator entities) {
-//        entities.forEachRemaining(entity -> {
-//            if (entity instanceof TenantBaseEntity) {
-//                log.debug("[post-flush] Updating the entity with tenant information: " + TenantHolder.getTenantId());
-//                ((TenantBaseEntity) entity).setTenantId(TenantHolder.getTenantId());
-//            }
-//        });
+//    public Boolean isTransient(Object entity) {
+//        if (entity instanceof TenantBaseEntity) {
+//            log.debug("[is-transient] Updating the entity with tenant information: " + TenantHolder.getTenantId());
+//            ((TenantBaseEntity) entity).setTenantId(TenantHolder.getTenantId());
+//        }
+//        return null;
 //    }
-
-
-    @Override
-    public Boolean isTransient(Object entity) {
-        if (entity instanceof TenantBaseEntity) {
-            log.debug("[is-transient] Updating the entity with tenant information: " + TenantHolder.getTenantId());
-            ((TenantBaseEntity) entity).setTenantId(TenantHolder.getTenantId());
-        }
-        return null;
-    }
-
-    @Override
-    public Object instantiate(String entityName, EntityMode entityMode, Serializable id) {
-        return null;
-    }
-
-    @Override
-    public int[] findDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
-        return null;
-    }
-
-    @Override
-    public String getEntityName(Object object) {
-        return null;
-    }
-
-    @Override
-    public Object getEntity(String entityName, Serializable id) {
-        return null;
-    }
-
-    @Override
-    public String onPrepareStatement(String sql) {
-        return sql;
-    }
-
-    @Override
-    public void onCollectionRemove(Object collection, Serializable key) throws CallbackException {
-    }
-
-    @Override
-    public void onCollectionRecreate(Object collection, Serializable key) throws CallbackException {
-    }
-
-    @Override
-    public void onCollectionUpdate(Object collection, Serializable key) throws CallbackException {
-    }
+//
+//    @Override
+//    public Object instantiate(String entityName, EntityMode entityMode, Serializable id) {
+//
+//        return null;
+//    }
+//
+//    @Override
+//    public int[] findDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
+//        if (entity instanceof TenantBaseEntity) {
+//            log.debug("[find-dirty] Updating the entity with tenant information: " + TenantHolder.getTenantId());
+//            ((TenantBaseEntity) entity).setTenantId(TenantHolder.getTenantId());
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public String getEntityName(Object object) {
+//        if (object instanceof TenantBaseEntity) {
+//            log.debug("[get-EntityName] Updating the entity with tenant information: " + TenantHolder.getTenantId());
+//            ((TenantBaseEntity) object).setTenantId(TenantHolder.getTenantId());
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public Object getEntity(String entityName, Serializable id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public String onPrepareStatement(String sql) {
+//        return sql;
+//    }
+//
+//    @Override
+//    public void onCollectionRemove(Object collection, Serializable key) throws CallbackException {
+//    }
+//
+//    @Override
+//    public void onCollectionRecreate(Object collection, Serializable key) throws CallbackException {
+//    }
+//
+//    @Override
+//    public void onCollectionUpdate(Object collection, Serializable key) throws CallbackException {
+//    }
 
 }
